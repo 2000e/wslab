291484b6ff4dab1bbf6129b6f8053c7b778a5d62
Steven Hobs <47906512+stevenhobs@users.noreply.github.com>
2024-10-10 16:45:43 +0800

    noble build

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
-apt autoremove -y
+apt purge -y cloud-init vim-tiny thunderbird snapd x11-apps libx11-6 libdrm2
+apt autoremove --purge -y
 apt autoclean -y
 apt clean -y
 rm -rf /var/lib/apt/lists/*
