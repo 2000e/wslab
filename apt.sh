dbd0309cf0aa6721f41e5dece3b466bd853b2c0f
Steven Hobs <47906512+stevenhobs@users.noreply.github.com>
2024-10-13 22:09:57 +0800

    优化镜像配置，添加open命令

--- apt.sh
+++ apt.sh
@@ -1,23 +1,4 @@
 echo "-- 更新 apt 源"
-cat <<'EOF' > /etc/apt/sources.list
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-deb https://mirror.nju.edu.cn/ubuntu/ jammy main restricted universe multiverse
-# deb-src https://mirror.nju.edu.cn/ubuntu/ jammy main restricted universe multiverse
-deb https://mirror.nju.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-# deb-src https://mirror.nju.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse
-deb https://mirror.nju.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-# deb-src https://mirror.nju.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse
-
-# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-deb https://mirror.nju.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
-# deb-src https://mirror.nju.edu.cn/ubuntu/ jammy-security main restricted universe multiverse
-
-# deb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-# # deb-src http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse
-
-# 预发布软件源，不建议启用
-# deb https://mirror.nju.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-# # deb-src https://mirror.nju.edu.cn/ubuntu/ jammy-proposed main restricted universe multiverse
-EOF
+sed -i 's|http://archive.ubuntu.com|https://mirror.nju.edu.cn|g' /etc/apt/sources.list
 apt update -y && apt upgrade -y
 apt install -y zsh unzip language-pack-zh-hans htop net-tools
